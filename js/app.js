11057411049ef9b2ee85223a8b98f8f48d5377b2
robcmills <robcmills@gmail.com>
2015-10-09 13:00:20 -0700

    add search,library routes, add first working search sumbit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.route('search', { path: '/search' });
+  this.route('library', { path: '/library' });
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
+
+App.SearchController = Ember.Controller.extend({
+  url: "http://www.omdbapi.com/?t=",
+  title: '',
+
+  actions: {
+    submit: function() {
+      console.log('submit search');
+      var title = this.get('title');
+      var url = this.get('url') + title; 
+      $.getJSON( url, function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          console.log('data:', data);
+          // items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+        // $( "<ul/>", {
+        //   "class": "my-new-list",
+        //   html: items.join( "" )
+        // }).appendTo( "body" );
+      });
+      return false;
+    }
   }
 });
